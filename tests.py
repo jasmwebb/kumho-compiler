246148a2095d8b3d6d33cfdb54efe1b43aa11f41
Jasmine Webb <jmwebb.94@gmail.com>
2021-03-05 14:43:33 -0800

    Setup tests file

---
+++ tests.py
@@ -0,0 +1,15 @@
+"""
+Tests for kumhocompiler.py
+"""
+import unittest
+
+
+class TestSequenceFunctions(unittest.TestCase):
+    """ This is one of potentially many TestCases """
+
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
